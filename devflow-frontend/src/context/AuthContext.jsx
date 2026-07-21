a31fc7df61a70acf86b911c75c3bb2d8b3c8047a
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-07-21 23:31:27 +0500

    feat: add jwt-decode for token handling and update AuthContext for user extraction

--- devflow-frontend/src/context/AuthContext.jsx
+++ devflow-frontend/src/context/AuthContext.jsx
@@ -1,28 +1,79 @@
 import { createContext, useContext, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = createContext();
+function getUserFromToken(token) {
+  const decodedToken = jwtDecode(token);
 
+  return {
+    id: decodedToken[
+      "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
+    ],
+    email:
+      decodedToken[
+        "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"
+      ],
+    role:
+      decodedToken[
+        "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
+      ],
+  };
+}
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
-   const [accessToken, setAccessToken] = useState(null);
-  const [refreshToken, setRefreshToken] = useState(null);
+
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem("accessToken")
+  );
+
+  const [refreshToken, setRefreshToken] = useState(
+    localStorage.getItem("refreshToken")
+  );
+
+  const [user, setUser] = useState(()=>{
+    const token = localStorage.getItem("accessToken");
+
+  return token ? getUserFromToken(token) : null;
+  });
+
+
 
   const login = (authData) => {
+
+    localStorage.setItem(
+      "accessToken",
+      authData.accessToken
+    );
+
+    localStorage.setItem(
+      "refreshToken",
+      authData.refreshToken
+    );
+
+
     setAccessToken(authData.accessToken);
     setRefreshToken(authData.refreshToken);
+    setUser(getUserFromToken(authData.accessToken));
   };
 
+
   const logout = () => {
+
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+
     setUser(null);
     setAccessToken(null);
     setRefreshToken(null);
   };
 
+
   const value = {
     user,
     setUser,
+
     accessToken,
     refreshToken,
+
     login,
     logout,
   };
@@ -34,6 +85,7 @@ export function AuthProvider({ children }) {
     </AuthContext.Provider>
   );
 }
+
 
 export function useAuth() {
   return useContext(AuthContext);
